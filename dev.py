360a198e7623e0f7684fbc9c597222ecd499f3e0
iamshyamala <devopslife99@gmail.com>
2022-11-14 03:13:44 +0530

    new file

---
+++ dev.py
@@ -0,0 +1,15 @@
+# Python Program to Find HCF or GCD
+# program to find H.C.F of two numbers using Loops
+
+# define a function
+def compute_hcf(x, y):
+
+# choose the smaller number
+    if x > y:
+        smaller = y
+    else:
+        smaller = x
+    for i in range(1, smaller+1):
+        if((x % i == 0) and (y % i == 0)):
+            hcf = i 
+    return hcf
